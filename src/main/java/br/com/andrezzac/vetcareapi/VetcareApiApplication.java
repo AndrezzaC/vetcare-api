b92ee1c6c3ca91b429b0c45d7aa51698ea884897
Andrezza Cardoso <andrezza.m.cardoso@gmail.com>
2026-07-03 22:20:12 -0300

    feat: cria estrutura inicial da API Spring Boot

---
+++ src/main/java/br/com/andrezzac/vetcareapi/VetcareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrezzac.vetcareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetcareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetcareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andrezzac/vetcareapi/VetcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrezzac.vetcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
