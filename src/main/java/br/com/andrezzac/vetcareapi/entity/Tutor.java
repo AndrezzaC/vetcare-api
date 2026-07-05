421ee5a342fff29a2f7aff3420a0b9e152e3a632
Andrezza Cardoso <andrezza.m.cardoso@gmail.com>
2026-07-04 21:00:54 -0300

    feat: adiciona entidade Tutor

---
+++ src/main/java/br/com/andrezzac/vetcareapi/entity/Tutor.java
@@ -0,0 +1,29 @@
+package br.com.andrezzac.vetcareapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "tutores")
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private List<String> telefones;
+    private String email;
+    private String endereco;
+    private String contatoPreferencial;
+
+}
